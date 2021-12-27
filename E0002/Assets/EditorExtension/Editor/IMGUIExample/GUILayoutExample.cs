5465b428b7aeb8ec31e715ed66b1930abbe5bfec
zhyq <1064639871@qq.com>
2021-12-27 16:58:53 +0800

    GUILayout 常用组件

--- E0002/Assets/EditorExtension/Editor/IMGUIExample/GUILayoutExample.cs
+++ E0002/Assets/EditorExtension/Editor/IMGUIExample/GUILayoutExample.cs
@@ -12,9 +12,59 @@ namespace EidtorExtension
             GetWindow<GUILayoutExample>().Show();
         }
 
+        private string _strTextField = "";
+        private string _strTextArea = "";
+        private int toolbarInt = 0;
+        string[] toolbarStrings = {"Toolbar1", "Toolbar2", "Toolbar3"};
         private void OnGUI()
         {
-            GUILayout.Label("Hello IMGUI");
+            GUILayout.BeginVertical("box");
+            {
+                GUILayout.BeginHorizontal();
+                {
+                    GUILayout.Label("Label:");
+                    GUILayout.Label("Hello");
+                }
+                GUILayout.EndHorizontal();
+
+                GUILayout.BeginHorizontal();
+                {
+                    GUILayout.Label("TextField:");
+                    _strTextField = GUILayout.TextField(_strTextField);
+                }
+                GUILayout.EndHorizontal();
+
+                GUILayout.BeginHorizontal();
+                {
+                    GUILayout.Label("TextArea:");
+                    _strTextArea = GUILayout.TextArea(_strTextArea);
+                }
+                GUILayout.EndHorizontal();
+                
+                GUILayout.BeginHorizontal();
+                {
+                    GUILayout.Label("Button:");
+                    if (GUILayout.Button("Button"))
+                    {
+                        Debug.Log("Button click");
+                    }
+                }
+                GUILayout.EndHorizontal();
+                
+                GUILayout.BeginHorizontal();
+                {
+                    GUILayout.Label("RepeatButton:");
+                    if (GUILayout.RepeatButton("RepeatButton"))
+                    {
+                        Debug.Log("RepeatButton click");
+                    }
+                }
+                GUILayout.EndHorizontal();
+
+                toolbarInt = GUILayout.Toolbar(toolbarInt, toolbarStrings);
+            }
+            GUILayout.EndVertical();
+
         }
     }
 }
